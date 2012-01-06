d26c880099a54e0517c674b3405cfef69f7866a7
Shalaj Jain <shalajj@codeaurora.org>
2012-01-05 17:58:57 -0800

    libI420colorconvert: Add new library for MovieStudio color conversions
    This library is a wrapper around the proprietary color conversion
    library. VideoEditor framework opens this library which in turn will
    open the proprietary color conversion library and perform the
    conversion. It has routines to handle color conversion from TILE4x2
    to Planar and from planar ro semi-planar.
    
    (cherry picked from commit dc4632471c096141709869cddfae0edd1feb2960)
    
    Change-Id: I138aefdf4bc7fe310c10c087b6c2abba441407c0
    CRs-Fixed: 328501

--- Android.mk
+++ Android.mk
@@ -4,4 +4,10 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
 	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
 endif
+ifeq ($(BOARD_USES_QCOM_LIBS),true)
+	include $(TOP)/vendor/qcom/opensource/omx/mm-core/Android.mk
+	include $(TOP)/vendor/qcom/opensource/omx/mm-video/Android.mk
 endif
+
+include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
+endif # BOARD_USES_QCOM_HARDWARE
