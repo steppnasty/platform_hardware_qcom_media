6d4e568e0cffe6939b6c7213df4570d45e556bba
steppnasty <steppnasty@gmail.com>
2016-01-29 15:36:00 -0500

    qcom/media: Updated to jb_mr1_rb4.19

--- Android.mk
+++ Android.mk
@@ -1,5 +1,18 @@
-ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
+QCOM_MEDIA_ROOT := $(call my-dir)
+ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7627a msm7630_surf apq8084,$(TARGET_BOARD_PLATFORM)),)
+include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
+include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
+endif
 
-include $(call all-subdir-makefiles)
+ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7630_surf apq8084,$(TARGET_BOARD_PLATFORM)),)
+include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
+endif
 
+ifneq ($(filter msm8610 msm8226 msm8974 msm8960 apq8084,$(TARGET_BOARD_PLATFORM)),)
+include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
+endif
+
+ifneq ($(filter msm8974 msm8960 msm8226 apq8084,$(TARGET_BOARD_PLATFORM)),)
+include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
+include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
 endif
